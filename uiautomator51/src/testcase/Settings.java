a93cedd2100ad2f666a18bf95a13b7d8e2135113
hansy1208 <hansy1208@thundersoft.com>
2016-03-17 15:33:47 +0800

    Add Settings case 359-366 and fix bug by Sanyang

--- uiautomator51/src/testcase/Settings.java
+++ uiautomator51/src/testcase/Settings.java
@@ -3318,6 +3318,10 @@ public class Settings extends UiAutomatorTestCase
 		//主体
 		SettingCommon.EnterPermission("其他权限");
 		excute(Object_Text, Operation_ClickWait, "读取电子邮件附件");
+		if((Boolean)excute(Object_ResIdInstance, Operation_IsChecked, "com.android.packageinstaller:id/switchWidget", "0"))
+		{
+			excute(Object_ResIdInstance, Operation_ClickWait, "com.android.packageinstaller:id/switchWidget", "0");
+		}
 		excute(Object_Device, Operation_PressMenu);
 		if((Boolean)excute(Object_Text, Operation_Exists, "显示系统应用"))
 		{
@@ -3484,8 +3488,8 @@ public class Settings extends UiAutomatorTestCase
 		{
 			excute(Object_Text, Operation_ClickWait, "显示系统进程");
 		}
-		check(Object_TextScroll, Operation_checkExist, "电话");
-		check(Object_TextScroll, Operation_checkExist, "工厂测试工具");
+		check(Object_TextScroll, Operation_checkExist, "电话", "vertical");
+		check(Object_TextScroll, Operation_checkExist, "工厂测试工具", "vertical");
 		//清场
 		excute(Object_Device, Operation_PressMenu);
 		excute(Object_Text, Operation_ClickWait, "隐藏系统进程");
@@ -3503,7 +3507,7 @@ public class Settings extends UiAutomatorTestCase
 		excute(Object_Text, Operation_ClickWait, "在其他应用的上层显示");
 		excute(Object_Device, Operation_PressMenu);
 		excute(Object_Text, Operation_ClickWait, "重置应用偏好设置");
-		check(Object_TextScroll, Operation_checkExist, "要重置应用偏好设置吗？");
+		check(Object_Text, Operation_checkExist, "要重置应用偏好设置吗？");
 	}
 	/**
 	 *修改系统设置
@@ -3516,7 +3520,7 @@ public class Settings extends UiAutomatorTestCase
 		excute(Object_TextScroll, Operation_ClickWait,"应用", "vertical");
 		excute(Object_ResourceId, Operation_ClickWait, "com.android.settings:id/advanced");
 		excute(Object_Text, Operation_ClickWait, "修改系统设置");
-		check(Object_TextScroll, Operation_checkExist, "可修改系统设置");
+		check(Object_Text, Operation_checkExist, "可修改系统设置");
 	}
 	/**
 	 *修改系统设置点击电话
@@ -3529,7 +3533,7 @@ public class Settings extends UiAutomatorTestCase
 		excute(Object_TextScroll, Operation_ClickWait,"应用", "vertical");
 		excute(Object_ResourceId, Operation_ClickWait, "com.android.settings:id/advanced");
 		excute(Object_Text, Operation_ClickWait, "修改系统设置");
-		excute(Object_Text, Operation_ClickWait, "电话");
+		excute(Object_TextScroll, Operation_ClickWait, "电话", "vertical");
 		check(Object_Text, Operation_checkExist, "修改系统设置");
 	}
 	/**
@@ -3543,7 +3547,7 @@ public class Settings extends UiAutomatorTestCase
 		excute(Object_TextScroll, Operation_ClickWait,"应用", "vertical");
 		excute(Object_ResourceId, Operation_ClickWait, "com.android.settings:id/advanced");
 		excute(Object_Text, Operation_ClickWait, "修改系统设置");
-		excute(Object_Text, Operation_ClickWait, "电话");
+		excute(Object_TextScroll, Operation_ClickWait, "电话", "vertical");
 		if((Boolean)excute(Object_ResourceId, Operation_IsChecked, "android:id/switchWidget"))
 		{
 			excute(Object_ResourceId, Operation_ClickWait, "android:id/switchWidget");
@@ -3564,7 +3568,7 @@ public class Settings extends UiAutomatorTestCase
 		excute(Object_TextScroll, Operation_ClickWait,"应用", "vertical");
 		excute(Object_ResourceId, Operation_ClickWait, "com.android.settings:id/advanced");
 		excute(Object_Text, Operation_ClickWait, "修改系统设置");
-		excute(Object_Text, Operation_ClickWait, "电话");
+		excute(Object_TextScroll, Operation_ClickWait, "电话", "vertical");
 		excute(Object_ResourceId, Operation_ClickWait, "com.android.settings:id/app_settings");
 		check(Object_Text, Operation_checkExist, "应用信息");
 	}
@@ -4421,6 +4425,113 @@ public class Settings extends UiAutomatorTestCase
 		check(Object_Text,Operation_checkExist,"中文 (繁體)");
 		check(Object_Text,Operation_checkExist,"中文 (简体)");
 		check(Object_Text,Operation_checkExist,"English");
+	}
+	/**
+	 * 关于手机
+	 * @throws UiObjectNotFoundException
+	 * @throws RemoteException
+	 */
+	public static void test_359() throws UiObjectNotFoundException, RemoteException 
+	{
+		//主体
+		excute(Object_TextScroll,Operation_ClickWait,"关于手机","vertical");
+		check(Object_Text,Operation_checkExist,"系统软件更新");
+	}
+	/**
+	 * 系统软件更新
+	 * @throws UiObjectNotFoundException
+	 * @throws RemoteException
+	 */
+	public static void test_360() throws UiObjectNotFoundException, RemoteException 
+	{
+		//主体
+		excute(Object_TextScroll,Operation_ClickWait,"关于手机","vertical");
+		excute(Object_Text,Operation_ClickWait,"系统软件更新");
+		check(Object_Text,Operation_checkExist,"系统软件更新");
+	}
+	/**
+	 * 状态信息
+	 * @throws UiObjectNotFoundException
+	 * @throws RemoteException
+	 */
+	public static void test_361() throws UiObjectNotFoundException, RemoteException 
+	{
+		//主体
+		excute(Object_TextScroll,Operation_ClickWait,"关于手机","vertical");
+		excute(Object_Text,Operation_ClickWait,"状态信息");
+		check(Object_Text,Operation_checkExist,"电池状态");
+	}
+	/**
+	 * 状态信息
+	 * @throws UiObjectNotFoundException
+	 * @throws RemoteException
+	 */
+	public static void test_362() throws UiObjectNotFoundException, RemoteException 
+	{
+		//主体
+		excute(Object_TextScroll, Operation_ClickWait, "关于手机", "vertical");
+		excute(Object_Text, Operation_ClickWait, "状态信息");
+		excute(Object_Text, Operation_ClickWait, "SIM卡状态");
+		check(Object_TextScroll, Operation_checkExist, "网络", "vertical");
+		check(Object_TextScroll, Operation_checkExist, "信号强度", "vertical");
+		check(Object_TextScroll, Operation_checkExist, "移动网络类型", "vertical");
+		check(Object_TextScroll, Operation_checkExist, "服务状态", "vertical");
+		check(Object_TextScroll, Operation_checkExist, "漫游", "vertical");
+		check(Object_TextScroll, Operation_checkExist, "移动网络状态", "vertical");
+		check(Object_TextScroll, Operation_checkExist, "IMEI", "vertical");
+		check(Object_TextScroll, Operation_checkExist, "IMEI SV", "vertical");
+	}
+	/**
+	 * 点击IMEI
+	 * @throws UiObjectNotFoundException
+	 * @throws RemoteException
+	 */
+	public static void test_363() throws UiObjectNotFoundException, RemoteException 
+	{
+		//主体
+		excute(Object_TextScroll, Operation_ClickWait, "关于手机", "vertical");
+		excute(Object_Text, Operation_ClickWait, "状态信息");
+		excute(Object_Text, Operation_ClickWait, "SIM卡状态");
+		excute(Object_TextScroll, Operation_ClickWait, "IMEI", "vertical");
+		check(Object_TextScroll, Operation_checkExist, "IMEI", "vertical");
+	}
+	/**
+	 * 法律信息
+	 * @throws UiObjectNotFoundException
+	 * @throws RemoteException
+	 */
+	public static void test_364() throws UiObjectNotFoundException, RemoteException 
+	{
+		//主体
+		excute(Object_TextScroll, Operation_ClickWait, "关于手机", "vertical");
+		excute(Object_Text, Operation_ClickWait, "法律信息");
+		check(Object_Text, Operation_checkExist, "开放源代码许可");
+	}
+	/**
+	 * 开放源代码许可
+	 * @throws UiObjectNotFoundException
+	 * @throws RemoteException
+	 */
+	public static void test_365() throws UiObjectNotFoundException, RemoteException 
+	{
+		//主体
+		excute(Object_TextScroll, Operation_ClickWait, "关于手机", "vertical");
+		excute(Object_Text, Operation_ClickWait, "法律信息");
+		excute(Object_Text, Operation_ClickWait, "开放源代码许可");
+		check(Object_Text, Operation_checkExist, "开放源代码许可");
+	}
+	/**
+	 * 系统 WebView 许可
+	 * @throws UiObjectNotFoundException
+	 * @throws RemoteException
+	 */
+	public static void test_366() throws UiObjectNotFoundException, RemoteException 
+	{
+		//主体
+		excute(Object_TextScroll, Operation_ClickWait, "关于手机", "vertical");
+		excute(Object_Text, Operation_ClickWait, "法律信息");
+		excute(Object_Text, Operation_ClickWait, "系统 WebView 许可");
+		check(Object_Text, Operation_checkExist, "系统 WebView 许可");
 	}
 	
 	/** 
