4a80039637a984004e3fc3d9284da5a5424cb39b
hansy1208 <hansy1208@thundersoft.com>
2016-04-27 15:15:59 +0800

    Add case by Sanyang

--- uiautomator51/src/testcase/Calendar.java
+++ uiautomator51/src/testcase/Calendar.java
@@ -1,13 +1,20 @@
 package testcase;
 
 import static framework.data.ObjectType.Object_Device;
+import static framework.data.ObjectType.Object_ResourceId;
+import static framework.data.ObjectType.Object_Text;
+import static framework.data.OperationType.Operation_GetBounds;
 import static framework.data.OperationType.Operation_WakeUp;
+import static framework.data.OperationType.Operation_checkExist;
 import static framework.data.ResIdTextAndDesc.*;
 import static framework.excute.Excute.ClearBackgroundApp;
 import static framework.excute.Excute.Wait;
+import static framework.excute.Excute.check;
 import static framework.excute.Excute.excute;
+import android.graphics.Rect;
 import android.os.RemoteException;
 
+import com.android.uiautomator.core.UiDevice;
 import com.android.uiautomator.core.UiObjectNotFoundException;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
@@ -37,5 +44,17 @@ public class Calendar extends UiAutomatorTestCase
 	{
 		CalendarCommon.switchMode("日程");
 	}
-
+	/**
+	 * 新建日程
+	 */
+	public static void test_019()
+	{
+		CalendarCommon.switchMode("日");
+		Rect ModArea = (Rect) excute(Object_ResourceId, Operation_GetBounds, "com.android.calendar:id/switcher");
+		int x = ModArea.centerX();
+		int y = ModArea.centerY();
+		UiDevice.getInstance().click(x / 2, y / 2);
+		UiDevice.getInstance().click(x / 2, y / 2);
+		check(Object_Text, Operation_checkExist, "完成");
+	}
 }
