f97babdd6405a81dcdf8ac13cb88d0a5334e4ed3
shiyh0122 <shiyh0122@thundersoft.com>
2016-05-11 14:30:44 +0800

    add case by yahui

--- uiautomator51/src/framework/common/CallFireWallCommon.java
+++ uiautomator51/src/framework/common/CallFireWallCommon.java
@@ -3,6 +3,7 @@ package framework.common;
 import static framework.data.ObjectType.*;
 import static framework.data.OperationType.*;
 import static framework.excute.Excute.*;
+import android.database.sqlite.SQLiteDatabase;
 
 public class CallFireWallCommon 
 {
@@ -23,4 +24,75 @@ public class CallFireWallCommon
 		excute(Object_Text, Operation_ClickWait,"确定");
 	}
 
+	/**
+	 * Description:添加电话拦截一条记录
+	 */
+	public static void fillIncomingCallData(){
+		String dbPath = "/data/data/com.sprd.firewall/databases/block.db"; 
+		SQLiteDatabase database = DeviceCommon.openDatabase(dbPath); 
+		DeviceCommon.deleteAllFromDatabase(database,"block_recorded");
+		DeviceCommon.insertToDatabase(database,"block_recorded","mumber_value,block_date","'10086',"+getDate("今天")+""); 
+		//int total = DeviceCommon.getRecordCountInDatabase(database,"block_recorded"); 
+		//System.out.println("call log'count : " + total); 
+		database.close();
+	}
+	
+	/**
+	 * Description:添加短信拦截一条记录
+	 */
+	public static void fillSMSData(){
+		String dbPath = "/data/data/com.sprd.firewall/databases/block.db"; 
+		SQLiteDatabase database = DeviceCommon.openDatabase(dbPath); 
+		DeviceCommon.deleteAllFromDatabase(database,"sms_block_recorded");
+		DeviceCommon.insertToDatabase(database,"sms_block_recorded","mumber_value,sms_content,block_date","'10086','123',"+getDate("今天")+""); 
+		//int total = DeviceCommon.getRecordCountInDatabase(database,"sms_block_recorded"); 
+		//System.out.println("call log'count : " + total); 
+		database.close();
+	}
+	/**
+	 * Description: 根据数据库中的时间毫秒转换为以秒来计算
+	 * @param day 前天  昨天  今天
+	 * @return Beforeyesterdaytime，Yesterdaytime，Todaytime;
+	 */
+	public static long getDate(String day) 
+	{
+		if (day.equals("前天"))
+		{
+			long Beforeyesterdaytime = System.currentTimeMillis()-2*24*3600*1000;
+			System.out.println(Beforeyesterdaytime);
+			return Beforeyesterdaytime;
+		}
+		else if(day.equals("昨天"))
+		{
+			long Yesterdaytime = System.currentTimeMillis()-1*24*3600*1000;
+			System.out.println(Yesterdaytime);
+			return Yesterdaytime;
+		} 
+		else if(day.equals("今天"))
+		{
+			long Todaytime = System.currentTimeMillis()-0*24*3600*1000;
+			System.out.println(Todaytime);
+			return Todaytime;
+		}
+		return 0;
+	}
+	
+	/**
+	 * 批量删除:选择模块：黑名单，电话记录，短信记录
+	 */
+	public static void BatchDelete(String type){
+		excute(Object_Text, Operation_ClickWait, type);
+		excute(Object_Description, Operation_ClickWait, "更多选项");
+		excute(Object_Text, Operation_ClickWait, "批量删除");
+		if(type.equals("电话记录")){
+			excute(Object_ResourceId, Operation_ClickWait, "com.sprd.firewall:id/Log_selete_all");
+		}else if(type.equals("短信记录")){
+			excute(Object_ResourceId, Operation_ClickWait, "com.sprd.firewall:id/Sms_Log_selete_all");
+		}
+		else if(type.equals("黑名单")){
+			excute(Object_ResourceId, Operation_ClickWait, "com.sprd.firewall:id/selete_all");
+		}	
+		excute(Object_Text, Operation_ClickWait, "完成");
+		excute(Object_Text, Operation_ClickWait, "确定");
+	}
 }
--- uiautomator51/src/testcase/CallFireWall.java
+++ uiautomator51/src/testcase/CallFireWall.java
@@ -10,7 +10,8 @@ import com.android.uiautomator.core.UiObjectNotFoundException;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 import framework.common.DeviceCommon;
-import framework.common.CallFireWallCommon;;
+import framework.common.CallFireWallCommon;
+import framework.common.MessageCommon;
 
 public class CallFireWall extends UiAutomatorTestCase
 {
@@ -29,5 +30,203 @@ public class CallFireWall extends UiAutomatorTestCase
     {
     }
 	
-
+	
+	/**
+	 * 进入电话记录
+	 */
+	public static void test_000()
+	{
+		//主体
+		CallFireWallCommon.fillIncomingCallData();
+		CallFireWallCommon.fillSMSData();
+	}
+	/**
+	 * 进入电话记录
+	 */
+	public static void test_104()
+	{
+		//前提
+		CallFireWallCommon.BatchDelete("电话记录");
+		//主体
+		check(Object_Text, Operation_checkExist, "电话拦截记录为空。");
+	}
+	
+	/**
+	 * 进入电话记录-批量删除
+	 */
+	public static void test_105()
+	{
+		//主体
+		excute(Object_Text, Operation_ClickWait, "电话记录");
+		excute(Object_Description, Operation_ClickWait, "更多选项");
+		check(Object_ClassName, Operation_EnabledFalse, "android.widget.LinearLayout");
+	}
+	
+	/**
+	 * 进入短信记录-批量删除
+	 */
+	public static void test_106()
+	{
+		//前提
+		CallFireWallCommon.BatchDelete("短信记录");
+		//主体
+		check(Object_Text, Operation_checkExist, "短信拦截记录为空。");
+	}
+	
+	/**
+	 * 进入短信记录-批量删除
+	 */
+	public static void test_107()
+	{
+		//主体
+		excute(Object_Text, Operation_ClickWait, "短信记录");
+		excute(Object_Description, Operation_ClickWait, "更多选项");
+		check(Object_ClassName, Operation_EnabledFalse, "android.widget.LinearLayout");
+	}
+	
+	/**
+	 * 进入电话记录-一条记录
+	 */
+	public static void test_025()
+	{
+		//主体
+		excute(Object_Text, Operation_ClickWait, "电话记录");
+		check(Object_ResourceId, Operation_checkExist, "com.sprd.firewall:id/ListView_customize");
+	}
+	
+	/**
+	 * 进入电话记录-长按一条记录
+	 */
+	public static void test_026()
+	{
+		//主体
+		excute(Object_Text, Operation_ClickWait, "电话记录");
+		excute(Object_ResIdInstance,Operation_LongClick , "com.sprd.firewall:id/ListView_customize","0");
+		check(Object_Text, Operation_checkExist, "删除");
+		check(Object_Text, Operation_checkExist, "详情");
+	}
+	
+	/**
+	 * 进入电话记录-长按一条记录，删除该记录
+	 */
+	public static void test_027()
+	{
+		try{
+			//主体
+			excute(Object_Text, Operation_ClickWait, "电话记录");
+			excute(Object_ResIdInstance,Operation_LongClick , "com.sprd.firewall:id/ListView_customize","0");
+			excute(Object_Text, Operation_ClickWait, "删除");
+			excute(Object_Text, Operation_ClickWait, "确定");
+			check(Object_Text, Operation_checkExist, "电话拦截记录为空。");
+		}
+		finally{
+			CallFireWallCommon.fillIncomingCallData();
+		}
+	}
+	
+	/**
+	 * 进入电话记录-长按一条记录-详情
+	 */
+	public static void test_028()
+	{
+		//主体
+		excute(Object_Text, Operation_ClickWait, "电话记录");
+		excute(Object_ResIdInstance,Operation_LongClick , "com.sprd.firewall:id/ListView_customize","0");
+		excute(Object_Text,Operation_ClickWait, "详情");
+		check(Object_ResourceId, Operation_TextContainsTrue, "android:id/message","姓名");
+		check(Object_ResourceId, Operation_TextContainsTrue, "android:id/message","号码");
+		check(Object_ResourceId, Operation_TextContainsTrue, "android:id/message","时间");
+	}
+	
+	/**
+	 * 进入电话记录-批量删除
+	 */
+	public static void test_029()
+	{
+		try{
+			//主体
+			excute(Object_Text, Operation_ClickWait, "电话记录");
+			excute(Object_Description, Operation_ClickWait, "更多选项");
+			excute(Object_Text, Operation_ClickWait, "批量删除");
+			excute(Object_ResIdInstance, Operation_ClickWait, "com.sprd.firewall:id/log_select","0");
+			excute(Object_Text, Operation_ClickWait, "完成");
+			excute(Object_Text, Operation_ClickWait, "确定");
+			check(Object_Text, Operation_checkExist, "电话拦截记录为空。");
+		}finally{
+			CallFireWallCommon.fillIncomingCallData();
+		}
+	}
+	
+	/**
+	 * 进入短信记录-长按一条记录
+	 */
+	public static void test_030()
+	{
+		//主体
+		excute(Object_Text, Operation_ClickWait, "短信记录");
+		check(Object_ResourceId, Operation_checkExist, "com.sprd.firewall:id/Sms_log_phone_name");
+	}
+	
+	/**
+	 * 进入短信记录-长按一条记录
+	 */
+	public static void test_031()
+	{
+		//主体
+		excute(Object_Text, Operation_ClickWait, "短信记录");
+		excute(Object_ResIdInstance,Operation_LongClick , "com.sprd.firewall:id/Sms_log_phone_name","0");
+		check(Object_Text, Operation_checkExist, "删除");
+		check(Object_Text, Operation_checkExist, "详情");
+	}
+	
+	/**
+	 * 进入短信记录-长按一条记录，删除该记录
+	 */
+	public static void test_032()
+	{
+		try{
+			//主体
+			excute(Object_Text, Operation_ClickWait, "短信记录");
+			excute(Object_ResIdInstance,Operation_LongClick , "com.sprd.firewall:id/Sms_log_phone_name","0");
+			excute(Object_Text, Operation_ClickWait, "删除");
+			excute(Object_Text, Operation_ClickWait, "确定");
+			check(Object_Text, Operation_checkExist, "短信拦截记录为空。");
+		}
+		finally{
+			CallFireWallCommon.fillSMSData();
+		}
+	}
+	
+	/**
+	 * 进入短信记录-长按一条记录-详情
+	 */
+	public static void test_033()
+	{
+		//主体
+		excute(Object_Text, Operation_ClickWait, "短信记录");
+		excute(Object_ResIdInstance,Operation_LongClick , "com.sprd.firewall:id/Sms_log_phone_name","0");
+		excute(Object_Text,Operation_ClickWait, "详情");
+		check(Object_ResourceId, Operation_TextContainsTrue, "android:id/message","姓名");
+		check(Object_ResourceId, Operation_TextContainsTrue, "android:id/message","号码");
+		check(Object_ResourceId, Operation_TextContainsTrue, "android:id/message","时间");
+	}
+	
+	/**
+	 * 进入短信记录-批量删除
+	 */
+	public static void test_034()
+	{
+		try{
+			//主体
+			excute(Object_Text, Operation_ClickWait, "短信记录");
+			excute(Object_Description, Operation_ClickWait, "更多选项");
+			excute(Object_Text, Operation_ClickWait, "批量删除");
+			excute(Object_ResIdInstance, Operation_ClickWait, "com.sprd.firewall:id/Sms_log_select","0");
+			excute(Object_Text, Operation_ClickWait, "完成");
+			excute(Object_Text, Operation_ClickWait, "确定");
+			check(Object_Text, Operation_checkExist, "短信拦截记录为空。");
+		}finally{
+			CallFireWallCommon.fillSMSData();
+		}
+	}
 }
