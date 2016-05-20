e4edd4a3203ee7a80fef59ffe5c4f5066aa0bee9
gaijy <gaijy0309@thundersoft.com>
2016-05-20 16:55:10 +0800

    Modify TestInstruction by Jinyu

---
+++ uiautomator51/getSIMID.bat
@@ -0,0 +1,6 @@
+echo off
+echo SIM1 is:
+adb shell sqlite3 /data/data/com.android.providers.telephony/databases/telephony.db "select icc_id from siminfo where sim_id=0"
+echo SIM2 is:
+adb shell sqlite3 /data/data/com.android.providers.telephony/databases/telephony.db "select icc_id from siminfo where sim_id=1"
+pause
